e97e39e9faedf31e70e74aab35d0fae4ba6bda41
marsr <richard.marsot@pega.com>
2019-11-30 11:57:42 -0500

    add integration tests

---
+++ tests/integration.test.js
@@ -0,0 +1,47 @@
+import puppeteer from 'puppeteer';
+
+const dir = process.cwd();
+const URL = `file://${dir}/docs/index.html`;
+//const URL = 'https://pegasystems.github.io/mashup-webcomponent/index.html';
+let browser;
+let page;
+
+describe('Integration testing of the Mashup Web Component', () => {
+  test(
+    'Open the main test page',
+    async () => {
+      browser = await puppeteer.launch({
+        args: ['--disable-web-security'],
+        headless: true,
+      });
+      page = await browser.newPage();
+
+      await page.goto(URL, { waitUntil: 'domcontentloaded' });
+
+      const title = await page.title();
+      expect(title).toBe('Pega Mashup Web Component');
+    },
+    process.env.PAGETIMEOUT,
+  );
+
+  test(
+    'Take screenshot of home page',
+    async () => {
+      await page.setViewport({ width: 1920, height: 1080 });
+      await page.screenshot({
+        path: './screenshots/home.jpg',
+        fullpage: true,
+        type: 'jpeg',
+      });
+    },
+    process.env.PAGETIMEOUT,
+  );
+
+  test(
+    'Close the browser when done',
+    async () => {
+      browser.close();
+    },
+    process.env.PAGETIMEOUT,
+  );
+});
